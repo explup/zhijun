005867968bf9553dcec5ff95dfe7aaf3a14086f8
explup <explup@gmail.com>
2016-07-04 07:40:48 +0200

    updated the bundles

--- ZhiJun/App_Start/BundleConfig.cs
+++ ZhiJun/App_Start/BundleConfig.cs
@@ -8,24 +8,36 @@ namespace ZhiJun
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/css/plugins/select_option1.css",
+                        "~/css/bootstrap/bootstrap.min.css",
+                        "~/fonts/font-awesome/css/font-awesome.min.css",
+                        "~/css/plugins/flexslider.css",
+                        "~/css/plugins/fullcalendar.min.css",
+                        "~/css/plugins/animate.css",
+                        "~/css/plugins/magnific-popup.css",
+                        "~/css/style.css",
+                        "~/css/colors/default.css",
+                        "~/js/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css",
+                        "~/js/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/js/jquery.min.js",
+                        "~/js/bootstrap/bootstrap.min.js",
+                        "~/js/plugins/jquery.flexslider.js",
+                        "~/js/plugins/jquery.selectbox-0.1.3.min.js",
+                        "~/js/plugins/jquery.magnific-popup.js",
+                        "~/js/plugins/waypoints.min.js",
+                        "~/js/plugins/jquery.counterup.js",
+                        "~/js/plugins/wow.min.js",
+                        "~/js/plugins/navbar.js",
+                        "~/js/plugins/moment.min.js",
+                        "~/js/plugins/fullcalendar.min.js",
+                        "~/js/custom.js",
+                        "~/js/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js",
+                        "~/js/plugins/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
